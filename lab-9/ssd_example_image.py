9ab19a6eebfffd994db1bd04d263444518328f10
Rahuldee <Rahuldee@gmail.com>
2025-09-16 13:47:22 -0700

    Update to LiteRT
    Update from TFLite to LiteRT

--- lab-9/ssd_example_image.py
+++ lab-9/ssd_example_image.py
@@ -1,7 +1,7 @@
 import time
 import cv2
 
-import tflite_runtime.interpreter as tflite
+from ai_edge_litert.interpreter import Interpreter
 
 import numpy as np
 
@@ -103,7 +103,7 @@ def detect_from_image():
     # Overview of Object Detection: https://www.tensorflow.org/lite/examples/object_detection/overview
     # Load pretrained model:https://tfhub.dev/tensorflow/lite-model/ssd_mobilenet_v1/1/default/1
      
-    interpreter = tflite.Interpreter(
+    interpreter = Interpreter(
         model_path="ssd_mobilenet_v1.tflite")
         
     interpreter.allocate_tensors()
